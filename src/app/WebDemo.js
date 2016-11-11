37537545882407730721c9b5884d7443648e4b7b
fricky_wang <fricky_wang@hotmail.com>
2016-11-11 17:09:01 +0800

    2016/11/11

---
+++ src/app/WebDemo.js
@@ -0,0 +1,3 @@
+angular.module('WebDemo', []);
+angular.module('WebDemo').controller('WebDemoCtrl',['$scope', function($scope){}]);
+angular.module('WebDemo').controller('HeaderCtrl',['$scope', function($scope){}]);
