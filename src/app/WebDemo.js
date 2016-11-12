f08caad5cb2cebfb1ad028cfe83ee969d2702742
FrickyWang <fricky_wang@hotmail.com>
2016-11-12 15:37:05 +0800

    2016/11/12

--- src/app/WebDemo.js
+++ src/app/WebDemo.js
@@ -1,3 +1,5 @@
 angular.module('WebDemo', []);
 angular.module('WebDemo').controller('WebDemoCtrl',['$scope', function($scope){}]);
-angular.module('WebDemo').controller('HeaderCtrl',['$scope', function($scope){}]);
+angular.module('WebDemo').controller('HeaderCtrl',['$scope', function($scope){
+	$scope.isAuthenticated = false;
+}]);
